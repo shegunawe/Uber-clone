34324269daaea576a933830d4def1d78ea3ce6eb
shegunawe <74549078+shegunawe@users.noreply.github.com>
2022-07-07 14:43:52 +0100

    commit

--- pages/login.js
+++ pages/login.js
@@ -11,7 +11,7 @@ const Login = () => {
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        router.push("/");
+        router.push("/login");
       }
     });
   }, []);
